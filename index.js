cc6f64bf0c7e7a9ca1d4fb4886ac8aad5bf5723a
AlmarwaniLa <laylaalmarwani.topgmail.com>
2021-02-15 15:56:16 -0800

    clucks

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const logger = require('morgan');
 const path = require('path');
+const cookieParser = require("cookie-parser");
+const router = require("./routes/clucks");
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const knex = require('knex');
+const multer = require('multer');
+const connect = require('connect');
+
 
 app.set('view engine', 'ejs');
-app.use(logger { "dev" });
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const clucksRouter = require('./routes/clucks');
+app.use('/clucks', clucksRouter);
 
 app.get("/", (req, res) => {
-    res.send("home");
+    res.render('home');
+
+    router.get('/login', (req, res) => {
+        res.render('login');
+    });
 });
 const DOMAIN = 'localhost';
 const PORT = '4646';
